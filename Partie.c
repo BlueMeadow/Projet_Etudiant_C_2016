15c2da17e7ad4e04731d8c9cb5b9359e88da900b
NOualet <nathanoualet@yahoo.fr>
2016-11-22 16:02:56 +0100

    Update Partie.c

--- Partie.c
+++ Partie.c
@@ -2,6 +2,14 @@
 #include<stdlib.h>
 #include "fonction.h"
 
+/*
+ La partie commence enfin. Etape importante: Veuillez entrer vos pseudos Première étape: sélection du premier joueur par la fonction QuiCommence.c. L'ordre des joueurs se fera par le
+ sens des aiguilles d'une montre. Donc le premier joueur joue. Le suivi des fonctions sera le même pour tous les joueurs de la partie. 
+ Tout d'abord, au premier lancer, les joueurs lancent leurs 5 dés. Ensuite ils continuent à lancer leurs dés de leur choix selon la combinaison
+ qu'il obtienne jusqu'au troisième lancer. Ils peuvent les lancers quand ils veulent. Pour chaque lancer, une aide sera disponible pour connaître 
+ le score qu'il obtienne selon la combinaison des 5 dés.  
+*/
+
 int De[5] /* stock les résultats des 5 dés */
 int Garder[5] /* tableau de booléens, permet de savoir si on veut relancer les dés */
 int Score[13][4] /* Garde le score de chaque joueur */
