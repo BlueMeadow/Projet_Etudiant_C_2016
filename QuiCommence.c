b62431e656a2ebcb51cac5c72d2a8a6e967ce123
BCombasteix <benboost@hotmail.fr>
2016-11-22 14:58:56 +0100

    fonction qui lance un de pour chaque joueur

--- QuiCommence.c
+++ QuiCommence.c
@@ -5,23 +5,40 @@
 int QuiCommence(int nb_joueur){
 	int joueur;
 	int i;
-	int com[4]={0,0,0,0};
+	int com[4]={1,1,1,1};
 	int max=0;
 	int retient;
+	int occ=1;
 	srand(time(NULL)); // initialisation de rand
 	for(joueur=0;joueur<nb_joueur;joueur++){
-		com[joueur] = rand()%6+1;
-		printf("Les dés sont %i.\n", com[joueur]);
+		if(com[joueur]>0){
+			com[joueur] = rand()%6+1;
+			printf("Le joueur %i a %i. ", joueur+1,com[joueur]);
+		}
+		if(max==com[joueur]){
+			occ++;
+			printf("le joueur %i a le meme score ",joueur+1);
+		}
 		if(max<com[joueur]){
 			max=com[joueur];
+			printf("le max est %i ",max);
 			retient=joueur;
+			occ=1;
+		}
+		if(joueur+1==nb_joueur && occ >1){
+			for(i=joueur;i>=0;i--){
+				if(max>com[i]){
+					com[i]=-1;
+				}
+			}
+			max=0;
+			joueur=-1;
 		}
 	}
 	retient++;
-	printf("le joueur %i ayant fais le plus gros score c'est lui qui commence.\n",retient);
+	printf("le joueur %i commence.\n",retient);
 	return retient;
 }
-
 int main(){
 	QuiCommence(4);
 }
