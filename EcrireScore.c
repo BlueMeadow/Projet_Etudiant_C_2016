c724d8beebc503f33a131b980d80e535fc73334f
BCombasteix <benboost@hotmail.fr>
2016-11-25 09:39:28 +0100

    affiche le score dans ZoneScore //a verifier//

---
+++ EcrireScore.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include "Yahtzee.h"
+
+void EcrireScore(int joueur){
+	int categorie = 0;
+	int x = 2;
+	int y = 2;
+	
+	mvwprintw(ZoneMessage ,y ,x ,"veuillez désigner la categorie choisie");
+	ChoixCategorie(categorie ,ZoneMessage); 
+	Calcul_Score();
+	
+	/* choix de la colonne */
+	x = 9 + 10 * joueur;
+	/* choix de la ligne */
+	
+	if(categorie < 7)
+		y = categorie + 2;
+	else
+		y = categorie + 5;
+	
+	/*affiche le score */
+	mvwprintw(ZoneScore ,x ,y ,"%i" ,Score[joueur][categorie]);
+}
+
+int main(){
+	EcrireScore(1){
+	
+	
