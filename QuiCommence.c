365a299881be1112f08e65ec33755953fe75d655
BCombasteix <benboost@hotmail.fr>
2016-11-25 09:47:50 +0100

    fonction pour savoir qui commence

--- QuiCommence.c
+++ QuiCommence.c
@@ -20,23 +20,28 @@ int QuiCommence(int nb_joueur){
 	
 	for( joueur = 0 ; joueur < nb_joueur; joueur++){
 		if(com[joueur] > 0){
-			//on lance le dé du joueur
+			//on lance le dé du joueur si son score est different de 0
 			com[joueur] = rand()%6+1;
 		}
+		//compte le nombre d'occurence du max
 		else if(max == com[joueur]){
 			occ++;
 		}
+		//change le max
 		else if(max < com[joueur]){
 			max = com[joueur];
 			retient = joueur;
+			//revien a une seule occurence
 			occ = 1;
 		}
+		//si il y a 2 fois le maximum et que les lancés sont fini recommence pour ces joueurs
 		else if(( joueur+1 == nb_joueur) && (occ > 1)){
 			for( i=joueur; i >= 0; i--){
 				if(max > com[i]){
 					com[i] = -1;
 				}
 			}
+			//reinitialisation du max et du compteur
 			max = 0;
 			joueur =- 1;
 		}
