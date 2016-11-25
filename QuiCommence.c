f9eb6a196b56d9aaa14d2e9c9031102013e01ee8
NOualet <nathanoualet@yahoo.fr>
2016-11-25 09:26:30 +0100

    ajout de commentaires, espaces

--- QuiCommence.c
+++ QuiCommence.c
@@ -1,38 +1,44 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 int QuiCommence(int nb_joueur){
+
 	int joueur;
 	int i;
-	int com[4]={1,1,1,1};
-	int max=0;
+	int com[4] = {1,1,1,1};
+	int max = 0;
 	int retient;
-	int occ=1;
-	int x =2;
-	int y=2;
+	int occ = 1;
+	int x = 2;
+	int y = 2;
+	
 	wclear(ZoneMessage);
-	srand(time(NULL)); // initialisation de rand
-	for(joueur=0;joueur<nb_joueur;joueur++){
-		if(com[joueur]>0){
+	//on initialise la zone qui contiendra le printf de la fin du programme
+	srand(time(NULL)); 
+	// initialisation de rand
+	
+	for( joueur = 0 ; joueur < nb_joueur; joueur++){
+		if(com[joueur] > 0){
+			//on lance le dé du joueur
 			com[joueur] = rand()%6+1;
 		}
-		if(max==com[joueur]){
+		else if(max == com[joueur]){
 			occ++;
 		}
-		if(max<com[joueur]){
-			max=com[joueur];
-			retient=joueur;
-			occ=1;
+		else if(max < com[joueur]){
+			max = com[joueur];
+			retient = joueur;
+			occ = 1;
 		}
-		if(joueur+1==nb_joueur && occ >1){
-			for(i=joueur;i>=0;i--){
-				if(max>com[i]){
-					com[i]=-1;
+		else if(( joueur+1 == nb_joueur) && (occ > 1)){
+			for( i=joueur; i >= 0; i--){
+				if(max > com[i]){
+					com[i] = -1;
 				}
 			}
-			max=0;
-			joueur=-1;
+			max = 0;
+			joueur =- 1;
 		}
 	}
 	retient++;
