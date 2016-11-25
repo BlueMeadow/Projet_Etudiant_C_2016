ff73c294d491e321e08049d869f93e07f138996c
FernandesSimon <vimairecarotte@yahoo.fr>
2016-11-25 09:28:10 +0100

    Update calcul.c

--- calcul.c
+++ calcul.c
@@ -1,63 +1,50 @@
-int occ[6]={0,0,0,0,0,0};
-int Calcul_Score(){
+
+int CalculScore(int * joueur){
+	int i;
 	int total=0;
-	switch(Categorie){//choix score a utiliser
-		
+	switch(Categorie){
+	/* choix score a utiliser */		
 		case 1 :
 		case 2 :
 		case 3 :
 		case 4 :
 		case 5 :
-		case 6 ://fonction qui calcule le score pour les dé de un a six
-			int i;
-			int occ[6]={0,0,0,0,0,0};
-			for(i=0;i<5;i++){
-				occ[De[i]-1]++;
-			}
-			total=Categorie*occ[Categorie];
-			Score[Categorie]=total;
-			break;
-			}
-			
+		case 6 :
+		/* fonction qui calcule le score pour les dés de un a six */
+			Score[*joueur][Categorie] = total;
+			break;			
 		case 7:
-			if(isBrelan()){
-				total=de[0]+De[1]+De[2]+De[3]+De[4]+De[5];
-				Score[7]=total;
-				break;
-			}
+			if(isBrelan())
+				Score[*joueur][Categorie] = de[0]+De[1]+De[2]+De[3]+De[4]+De[5];
+			break;
+			
 		case 8:
 			if(isCarre()){
-				total=De[0]+De[1]+De[2]+De[3]+De[4]+De[5];
-				Score[8]=total;
-				break;
-			}
+				Score[*joueur][Categorie] = De[0]+De[1]+De[2]+De[3]+De[4]+De[5];
+			break;
+			
 		case 9:
 			if(isFull()){
-				total=25;
-				Score[9]=total;
-				break;
-			}
+				Score[*joueur][Categorie] = 25;
+			break;
+			
 		case 10:
 			if(isPtSuite()){
-				total=30;
-				Score[10]=total;
-				break;
-			}
+				Score[*joueur][Categorie] = 30;
+			break;
+			
 		case 11:
 			if(isGdSuite()){
-				total=40;
-				Score[11]=total;
-				break;
-			}
+				Score[*joueur][Categorie] = 40;
+			break;
+			
 		case 12:
 			if(isYahtzee()){
-				total=50;
-				Score[12]=total;
-				break;
-			}
-		case 13://case chance
-			total= De[0]+De[1]+De[2]+De[3]+De[4]+De[5];
-			Score[13]=total;
+				Score[*joueur][Categorie] = 50;
+			break;
+			
+		case 13:
+			Score[*joueur][Categorie] = De[0]+De[1]+De[2]+De[3]+De[4]+De[5];
 			break;
 		
 		
