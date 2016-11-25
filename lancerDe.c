b7db9ce7a49940bfa0b6fdc0438ac233df799bb5
BCombasteix <benboost@hotmail.fr>
2016-11-25 09:53:01 +0100

    valeur aleatoire dans De[]

--- lancerDe.c
+++ lancerDe.c
@@ -6,9 +6,11 @@
 int De[5] = {0, 0 , 0 , 0, 0};
 
 int main(){
+	//pour que se soit vraiment aleatoire
 	srand(time(0));
 	for(int i = 0; i < 5; i++){
 		if(garder[i] == 0){	
+			//choisie un chiffre entre 1 et 6 
 			De[i] = rand()%6+1;
 			printf("Les dés sont %i.\n", De[i]);
 		}
