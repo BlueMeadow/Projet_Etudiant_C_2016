e4beafd34860e085b8e12704069eb2456c665770
NOualet <nathanoualet@yahoo.fr>
2016-11-25 09:33:48 +0100

    Update and rename EnterPseudo.c to EntrerPseudo.c

--- EntrerPseudo.c
+++ EntrerPseudo.c
@@ -2,13 +2,12 @@
 #include <string.h>
 #include <stdio.h>
 
-void EnterPseudo(){
+void EntrerPseudo(){
 
 	FILE *fic1;
-	//FILE *fic2;
 	
 	char nom[10];
-	//char pseudo[10];
+	
 	int nbre_joueurs = 4;
 	int compteur = 1;
 	int x=2;
@@ -18,44 +17,25 @@ void EnterPseudo(){
 	
 	do{
 		wclear(ZoneMessage);
-		// On demande l'âge
+		// On demande le pseudo du joueur
 		mvwprintw(ZoneMessage, y, x,"Quel est votre pseudo (10 caractères maximum) ? ");
 		scanf("%s", nom);
 		
 		if ( strlen(nom) <= 10){
-			// On l'écrit dans le fichier
+			// Si les conditions d'écriture sont respectés, on écrit le pseudo dans le fichier
 			wclear(ZoneMessage);
 			fprintf(fic1, "Le joueur numéro %i s'appelle %s.\n", compteur, nom);
 			compteur++;
 	 	}
 	 	else{
+			//si les conditions ne sont pas respectés, on affiche un message d'erreur
 	 		mvwprintw(ZoneMessage, y, x, "Veuillez respecter les conditions d'écriture, s'il vous plaît.\n");
 	 	}
-	 			
-		
 	
 	}while(compteur <= nbre_joueurs);
+	//la demande de aisie de pseudo s'arrete lorsque tous les joueurs les ont.
 	
-	fclose(fic1);
-	
-	/*fic2 = fopen("fichierpseudo.txt","r");
-	
-	if(fic2 == NULL){
-		printf("Le fichier pseudo est vide.\n");
-	}
-	else{
-		while(!feof(fic2)){
-		
-			fscanf(fic2,"%s", pseudo); // On lit le caractère
-            		fprintf("%s", pseudo); // On l'affiche
-		
-		}
-		fclose(fic2);
-	
-	
-	}*/
-	
-
+	fclose(fic1);	
 }
 
 
