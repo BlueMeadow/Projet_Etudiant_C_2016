57251dd30e5477d6119a18f29154387efe9c9b29
FernandesSimon <vimairecarotte@yahoo.fr>
2016-11-28 10:19:45 +0100

    A tester

--- MenuDeroulant.c
+++ MenuDeroulant.c
@@ -1,49 +1,48 @@
-
-
 void Defilement (){
 /* A faire dans une fenetre ayant moins de lignes que le texte que l'on veut afficher */
 	int x, y, ch;
-	
-	x = 1;
+	WINDOW * ZoneRegles;
+	ZoneRegles =  newwin(30, 10, 1, 1);	
+	wrefresh(ZoneRegles);
+	keypad(ZoneRegles, TRUE);
 	y = 1;
 	
 	do
 	{
-		ch = wgetch(ZoneMenu);
-		switch(ch)
+
+		wclear(ZoneRegles);
+		/* Copier Les règles ici */
+		wrefresh(ZoneRegles);
+		ch = wgetch(ZoneRegles);
+		switch (ch)
 		{
-			case KEY_UP : if (y > 1) y--; break;
-			case KEY_DOWn : if (y < 52 - LINES) y++; break;
+			case KEY_UP:  
+				if ( y > -24 )
+				{
+					y = y-1;
+				}
+				break;
+			case KEY_DOWN:
+				if ( y < 1)
+				{
+					y = y + 1;
+
+				}
+				break;
 		}
-		/* METTRE TOUTES LES LIGNES A AFFICHER ICI */
-	} while (ch != 10) /* Entrée fait sortir de l'affichage des règles */
+
+	} while (ch != 10); /* Entrée fait sortir de l'affichage des règles */
 	/* Retour au menu ici */
 }
 
 
-void PageParPage (){
-/* A faire dans une fenetre dont le nombre de ligne est supérieur à celui de toutes les pages */
-	int ch, choix;
-	
-	choix = 1; /* Page 1 affichée par defaut */
-	
-	do
-	{
-		switch(choix)
-		{
-			case 1 : Page1(); break;
-			case 2 : Page2(); break;
-			case 3 : Page3(); break;
-		}
-		ch = wgetch(ZoneMenu);
-		switch(ch)
-		{
-			
-			case KEY_UP : if (choix > 1) choix--; break;
-			case KEY_DOWN : if (y < 3) choix++; break;
-		}
-		/* METTRE TOUTES LES LIGNES A AFFICHER ICI */
-	} while (ch != 10) /* Entrée fait sortir de l'affichage des règles */
-	/* Retour au menu ici */
-}
 
+int main()
+{
+	int ch;
+	initscr();
+	cbreak();
+	noecho(); /* Les trois lignes au dessus sont dan MiseEnPlace() */
+	Defilement();
+	endwin();
+}
