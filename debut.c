4678e5edd7b78bd9cb45458a1c5d6eeddc98e421
NOualet <nathanoualet@yahoo.fr>
2016-12-01 12:03:02 +0100

    debut.c -- ne fonctionne pas

--- debut.c
+++ debut.c
@@ -10,20 +10,18 @@ int debut(WINDOW *localwin, int y, int x){
 /* Affichage du menu et saisie du choix */
 		
 		
-	mvwprintw(localwin, y,   x,  " Bonjour Joueurs de Yahtzee !");
-	mvwprintw(localwin, y+1, x,  " 1 - Nouvelle Partie ?");
-	mvwprintw(localwin, y+2, x,  " 2 - Voir les Regles.");
-	mvwprintw(localwin, y+3, x,  " 3 - Quitter le jeu.");
+	mvwprintw(localwin, y,   x,  "Bonjour Joueurs de Yahtzee !");
+	mvwprintw(localwin, y+1, x,  "  | Nouvelle Partie ?");
+	mvwprintw(localwin, y+2, x,  "  | Voir les Regles.");
+	mvwprintw(localwin, y+3, x,  " | Quitter le jeu.");
 	
 }
 
-void ChoixCategorie (int *Categorie, WINDOW *localwin)
+void ChoixCategorie ( WINDOW *localwin)
 {
-	
-	
 	int y, x;
-	y = 1; //indice d'ordonnée
-	x = 1; // indice d'abscisse
+	y = 5; //indice d'ordonnée
+	x = 4; // indice d'abscisse
 	int ch; 
 	keypad(localwin, TRUE); //Permet l'utilisation des touches directionnelles
 	
@@ -33,51 +31,91 @@ void ChoixCategorie (int *Categorie, WINDOW *localwin)
 	
 	do 
 	{
-		wclear(localwin);
-		/* Copier Les règles ici */
-		debut(localwin, y, x);
-		wrefresh(localwin);
-		
 		ch = wgetch(localwin);
 		switch (ch)
 		{
 			case KEY_UP:  
-				
-				y--;
-				/* On remonte d'une case */
-				wmove(localwin, y, x);
-				wrefresh(localwin);
+				/* Fleche du haut entree */
+				if (y == 5)
+				{
+					y = 20; 
+					/* Si on est en haut du tableau, on passe en bas */
+					wmove(localwin, y, x);
+					wrefresh(localwin);
+				}
+				else if (y == 14)
+				{
+					/* On remet un vide car on change de catégorie */
+					y = 10; 
+					/* On passe les 3 lignes centrales du tableau de score */
+					wmove(localwin, y, x);
+					wrefresh(localwin);
+				}
+				else 
+				{
+					y--;
+					/* On remonte d'une case */
+					wmove(localwin, y, x);
+					wrefresh(localwin);
+				}
 				break;
-				
-				
 			
 			case KEY_DOWN:
 				/* Fleche du bas entree */
-				y++;
-				/* On descend d'une case */
-				wmove(localwin, y, x);
-				wrefresh(localwin);
-				
+				if (y == 20)
+				{
+					y = 5; 
+					/* Si on est en bas du tableau, on passe en haut */
+					wmove(localwin, y, x);
+					wrefresh(localwin);
+				}
+				else if (y == 10)
+				{
+					y = 14; 
+					/* On passe les 3 lignes centrales du tableau de score */
+					wmove(localwin, y, x);
+					wrefresh(localwin);
+				}
+				else 
+				{
+
+					y++;
+					/* On descend d'une case */
+					wmove(localwin, y, x);
+					wrefresh(localwin);
+				}
 				break;
 		}
-		if (y < 11) *Categorie = y - 4;
-		else *Categorie = y - 7;
+		
+			
 	} while (ch != 10); 
 		/* Entrée valide le choix */
 }
 
 int main(void){
-
-	int x, y, ch;
-	WINDOW * ZoneDebut;
-	ZoneDebut =  newwin(30, 67, 1, 1);	
 	
-	ChoixCategorie (1, ZoneDebut);
-
+	int ch;
+	WINDOW * ZoneDebut;
+	ZoneDebut =  newwin(20, 40, 1, 1);
+	initscr();
+	cbreak();
+	noecho(); /* Les trois lignes au dessus sont dans MiseEnPlace() */
+	ChoixCategorie(ZoneDebut);
+	endwin();
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
