ce5d3b4bb3b72df9fdb1aaeae6f84da49c20aebf
FernandesSimon <vimairecarotte@yahoo.fr>
2016-12-01 08:50:50 +0100

    Update interface.c

--- interface.c
+++ interface.c
@@ -1,50 +1,71 @@
-
-
 #include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <time.h>
 
-int De[5]={1,2,3,4,5};
-int Garde[5]={0};
-char pseudo_j1[30] = "J1";
-char pseudo_j2[30] = "J2;
-char pseudo_j3[30] = "J3";
-char pseudo_j4[30] = "J4";
-int Categorie;
-WINDOW *ZoneDe;
-WINDOW *ZoneMessage;
-WINDOW *ZoneAide;
-WINDOW *ZoneScore;
-char pseudos[50];
-
-
-/*
-
-|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-
-*/
+int De[5]={0};                                  /* Tableau de dés */
+int Garde[5]={0};                               /* Tableau booléen, sert à savoir si on garde les dés ou si on les relance */
+int Occurence[6]={0};                           /* Tableau content le nombre d'occurence de chaque valeur de dé */
+char pseudo_j1[10] = "J1";                      /* Pseudo du joueur 1 */
+char pseudo_j2[10] = "J2";                      /* Pseudo du joueur 2 */
+char pseudo_j3[10] = "J3";                      /* Pseudo du joueur 3 */
+char pseudo_j4[10] = "J4";                      /* Pseudo du joueur 4 */
+char pseudos[40];                               /* Chaine de caractère contenant les pseudos des 4 joueurs avec le nombre d'espace nécesaire à un affichage correct */
 
 
 
-WINDOW *create_newwin(int height, int width, int starty, int startx)
+int Categorie;                                   /* Permet de savoir quelle catégorie a été sélectionnée pour calculer le score */
+WINDOW *ZoneTitre;                               /* Sous fenetre pour l'affichage de YAHTZEE ! */
+WINDOW *ZoneMenu;                                /* Sous fenetre pour l'affichage des menus */
+WINDOW *ZoneDe;                                  /* Sous fenetre pour l'affichage de la valeur de dés */
+WINDOW *ZoneMessage;                             /* Sous fenetre pour l'affichage des messages à l'utilisateur pendant la partie */
+WINDOW *ZoneAide;                                /* Sous fenetre pour l'affichage de l'aide au jeu */
+WINDOW *ZoneScore;				                 /* Sous fenetre pour l'affichage de la fiche de score */
+
+
+/* Menu */
+
+void Debut();                                     /* Premier menu, permet de commencer une partie, afficher les règles ou quitter */
+void Partie();                                    /* Commence une partie de Yahtzee */
+void Regles();                                    /* Affiche les règles */       
+
+/* Initialisation de la partie */
+
+
+void EntrerPseudo();                                   /* Demande les pseudos des joueurs */
+void MiseEnPlace();                                    /* Crée les fenêtres utilisées pour jouer */
+void FicheDeScore();								   /* Affiche la fiche de score dans la fenêtre */
+
+/* Fonctions de jeu */
+
+void LancerDe();                                 /* Simule 5 lancers de dés et entre les résultats dans le tableau */
+void CalculOccurence();                          /* Entre l'occurence de chaque valeur dans le tableau */
+void Garder();                                   /* Permet au joueur de conserver de dés avant de relancer */
+int isBrelan();									 /* Fonctions booléennes vérifiant les résultats */
+int isCarre();									 /* --- */
+int isFull();                                    /* --- */
+int isPtSuite();                                 /* --- */
+int isGdSuite();                                 /* --- */
+int isYahtzee();                                 /* --- */
+void ChoixCategorie(int *Categorie);             /* Permet de choisir la catégorie avec les flèches */
+void CalculScore();                              /* Calcul le score pour la catégorie choisie */
+void EcrireScore();                              /* Ecrie le score sur la ligne et dans la colonne correspondant au joueur */
+
+
+
+WINDOW *CreerFenetre(int height, int width, int starty, int startx)
 {	WINDOW *localwin;
 
 	localwin = newwin(height, width, starty, startx);
-	wborder(localwin, '|', '|', '-', '-', '+', '+', '+', '+');		/* 0, 0 gives default characters 
-					 * for the vertical and horizontal
-					 * lines			*/
-	wrefresh(localwin);		/* Show that box 		*/
+	wborder(localwin, '|', '|', '-', '-', '+', '+', '+', '+');		
+	wrefresh(localwin);	
 
 	return localwin;
 }
 
 
-
-
-WINDOW *create_newwin(int height, int width, int starty, int startx);
-void destroy_win(WINDOW *local_win);
 
 void ChainePseudo (char pseudos[50])
 {
@@ -93,68 +114,73 @@ void FicheDeScore(WINDOW *localwin, int y, int x)
 
 
 
-void de_un(WINDOW *localwin, int y, int x){
+void DeUn(WINDOW *localwin, int y, int x){
 /* Dessine un de de valeur 1 */ 
-	mvwprintw(localwin, y, x, " +-------+"); wrefresh(localwin);
-	mvwprintw(localwin, y+1, x, " |       |"); wrefresh(localwin);
-	mvwprintw(localwin, y+2, x, " |   O   | [   ]"); wrefresh(localwin);
-	mvwprintw(localwin, y+3, x, " |       |"); wrefresh(localwin);
-	mvwprintw(localwin, y+4, x, " +-------+"); wrefresh(localwin);
+	mvwprintw(localwin, y, x, " +-------+");
+	mvwprintw(localwin, y+1, x, " |       |");
+	mvwprintw(localwin, y+2, x, " |   O   | [   ]");
+	mvwprintw(localwin, y+3, x, " |       |");
+	mvwprintw(localwin, y+4, x, " +-------+"); 
 	wrefresh(localwin);
 }
 						
-void de_deux(WINDOW *localwin, int y, int x){
+void DeDeux(WINDOW *localwin, int y, int x){
 /* Dessine un de de valeur 2 */ 
-	mvwprintw(localwin, y, x, " +-------+"); wrefresh(localwin);
-	mvwprintw(localwin, y+1, x, " | O     |"); wrefresh(localwin);
-	mvwprintw(localwin, y+2, x, " |       | [   ]"); wrefresh(localwin);
-	mvwprintw(localwin, y+3, x, " |     O |"); wrefresh(localwin);
-	mvwprintw(localwin, y+4, x, " +-------+"); wrefresh(localwin);
+	mvwprintw(localwin, y, x, " +-------+");
+	mvwprintw(localwin, y+1, x, " | O     |");
+	mvwprintw(localwin, y+2, x, " |       | [   ]");
+	mvwprintw(localwin, y+3, x, " |     O |");
+	mvwprintw(localwin, y+4, x, " +-------+"); 
+	wrefresh(localwin);
 }
 					
-void de_trois(WINDOW *localwin, int y, int x){
+void DeTrois(WINDOW *localwin, int y, int x){
 /* Dessine un de de valeur 3 */ 
-	mvwprintw(localwin, y, x, " +-------+"); wrefresh(localwin);
-	mvwprintw(localwin, y+1, x, " | O     |"); wrefresh(localwin);
-	mvwprintw(localwin, y+2, x, " |   O   | [   ]"); wrefresh(localwin);
-	mvwprintw(localwin, y+3, x, " |     O |"); wrefresh(localwin);
-	mvwprintw(localwin, y+4, x, " +-------+"); wrefresh(localwin);
+	mvwprintw(localwin, y, x, " +-------+");
+	mvwprintw(localwin, y+1, x, " | O     |");
+	mvwprintw(localwin, y+2, x, " |   O   | [   ]");
+	mvwprintw(localwin, y+3, x, " |     O |");
+	mvwprintw(localwin, y+4, x, " +-------+");
+	wrefresh(localwin);
 }
 					
-void de_quatre(WINDOW *localwin, int y, int x){
+void DeQuatre(WINDOW *localwin, int y, int x){
 /* Dessine un de de valeur 4 */ 
 	mvwprintw(localwin, y, x, " +-------+");
-	mvwprintw(localwin, y+1, x, " | O   O |"); wrefresh(localwin);
-	mvwprintw(localwin, y+2, x, " |       | [   ]"); wrefresh(localwin);
-	mvwprintw(localwin, y+3, x, " | O   O |"); wrefresh(localwin);
-	mvwprintw(localwin, y+4, x, " +-------+"); wrefresh(localwin);
+	mvwprintw(localwin, y+1, x, " | O   O |");
+	mvwprintw(localwin, y+2, x, " |       | [   ]");
+	mvwprintw(localwin, y+3, x, " | O   O |");
+	mvwprintw(localwin, y+4, x, " +-------+"); 
+	wrefresh(localwin);
 }					
-void de_cinq(WINDOW *localwin, int y, int x){
+void DeCinq(WINDOW *localwin, int y, int x){
 /* Dessine un de de valeur 5 */ 
-	mvwprintw(localwin, y, x, " +-------+"); wrefresh(localwin); 
-	mvwprintw(localwin, y+1, x, " | O   O |"); wrefresh(localwin);
-	mvwprintw(localwin, y+2, x, " |   O   | [   ]"); wrefresh(localwin);
-	mvwprintw(localwin, y+3, x, " | O   O |"); wrefresh(localwin);
-	mvwprintw(localwin, y+4, x, " +-------+"); wrefresh(localwin);
+	mvwprintw(localwin, y, x, " +-------+");
+	mvwprintw(localwin, y+1, x, " | O   O |");
+	mvwprintw(localwin, y+2, x, " |   O   | [   ]");
+	mvwprintw(localwin, y+3, x, " | O   O |");
+	mvwprintw(localwin, y+4, x, " +-------+"); 
+	wrefresh(localwin);
 }
 					
-void de_six(WINDOW *localwin, int y, int x){
+void DeSix(WINDOW *localwin, int y, int x){
 /* Dessine un de de valeur 6 */ 
-	mvwprintw(localwin, y, x, " +-------+"); wrefresh(localwin);
-	mvwprintw(localwin, y+1, x, " | O   O |"); wrefresh(localwin);
-	mvwprintw(localwin, y+2, x, " | O   O | [   ]"); wrefresh(localwin);
-	mvwprintw(localwin, y+3, x, " | O   O |"); wrefresh(localwin);
-	mvwprintw(localwin, y+4, x, " +-------+"); wrefresh(localwin);
+	mvwprintw(localwin, y, x, " +-------+");
+	mvwprintw(localwin, y+1, x, " | O   O |");
+	mvwprintw(localwin, y+2, x, " | O   O | [   ]");
+	mvwprintw(localwin, y+3, x, " | O   O |");
+	mvwprintw(localwin, y+4, x, " +-------+"); 
+	wrefresh(localwin);
 }
 
-void de_vide(WINDOW *localwin, int y, int x)
+void DeVide(WINDOW *localwin, int y, int x)
 /* Dessine un de si on a pas encore de valeur */
 {
-mvwprintw(localwin, y, x, " +-------+"); wrefresh(localwin);
-	mvwprintw(localwin, y+1, x, " |       |"); wrefresh(localwin);
-	mvwprintw(localwin, y+2, x, " |   ?   | [   ]"); wrefresh(localwin);
-	mvwprintw(localwin, y+3, x, " |       |"); wrefresh(localwin);
-	mvwprintw(localwin, y+4, x, " +-------+"); wrefresh(localwin);
+	mvwprintw(localwin, y, x, " +-------+");
+	mvwprintw(localwin, y+1, x, " |       |");
+	mvwprintw(localwin, y+2, x, " |   ?   | [   ]");
+	mvwprintw(localwin, y+3, x, " |       |");
+	mvwprintw(localwin, y+4, x, " +-------+");
 	wrefresh(localwin);
 }
 
@@ -167,40 +193,42 @@ void AffichageDe(int De[5], WINDOW *localwin)
 	{
 		switch(De[i])
 		{
-			case 0 : de_vide(localwin, y, x); break;
-			case 1 : de_un(localwin, y, x); break;
-			case 2 : de_deux(localwin, y, x); break;
-			case 3 : de_trois(localwin, y, x); break;
-			case 4 : de_quatre(localwin, y, x); break;
-			case 5 : de_cinq(localwin, y, x); break;
-			case 6 : de_six(localwin, y, x); break;
+			case 0 : DeVide(localwin, y, x); break;
+			case 1 : DeUn(localwin, y, x); break;
+			case 2 : DeDeux(localwin, y, x); break;
+			case 3 : DeTrois(localwin, y, x); break;
+			case 4 : DeQuatre(localwin, y, x); break;
+			case 5 : DeCinq(localwin, y, x); break;
+			case 6 : DeSix(localwin, y, x); break;
 		}
 		y+=5;
 	}
 }
 
 
-void MiseEnPlace()
+void InterfaceJeu()
 {	
 	initscr();
 	cbreak();
 	noecho();
 	refresh();
 	ChainePseudo(pseudos);
-	ZoneAide = create_newwin(30,30,3,92);
-	ZoneDe = create_newwin(31, 19, 3, 3);
-	ZoneScore = create_newwin(25,70,3,22);
-	ZoneMessage = create_newwin(10,70,28,22);
-	FicheDeScore(ZoneScore,1,2);
+	ZoneAide = CreerFenetre(31,30,3,(COLS-70)/2+70);
+	ZoneDe = CreerFenetre(31, 19, 3, (COLS-70)/2-19);
+	ZoneScore = CreerFenetre(25,70,3, (COLS-70)/2);
+	ZoneMessage = CreerFenetre(10,70,28,(COLS-70)/2);
+	FicheDeScore(ZoneScore,1,6);
 	AffichageDe(De, ZoneDe);
+	wmove(ZoneDe, 100, 67); /* Sort le curseur de la fenetre, purement cosmétique */
+	wrefresh(ZoneDe);
 }
 
 
-/*int main ()
+int main ()
 {
 	int ch = 68;
-	MiseEnPlace();
-	AffichageDe(De, ZoneDe);
+	InterfaceJeu();
+	//AffichageDe(De, ZoneDe);
 	while (ch != 10)
 		ch = getch();
 	De[3] = 6;
@@ -209,4 +237,4 @@ void MiseEnPlace()
 	while (ch != 10)
 		ch = getch();
 	endwin();
-}*/
+}
