05182cda1a8eb5889521d32b3755bbe79062e6cc
NOualet <nathanoualet@yahoo.fr>
2016-12-01 09:26:50 +0100

    changerjoueur.c -- ajout comment doxygen

--- changerjoueur.c
+++ changerjoueur.c
@@ -1,3 +1,12 @@
+/**
+* \file changerjoueur.c
+* \brief Permet de changer de numéro de joueur
+* \author Benoit COMBASTEIX
+* \date 20 novembre 2016
+* \fn ChangerJoueur()
+* \return Livre le numéro de joueur qui suit celui qui a joué
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
