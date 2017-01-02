56d34a8bdb06b27b62c3dab301bacfd6b71d88bd
FernandesSimon <vimairecarotte@yahoo.fr>
2017-01-02 23:45:40 +0100

    Update Affichage.h

--- Projet/include/Affichage.h
+++ Projet/include/Affichage.h
@@ -4,14 +4,14 @@
 void EcranTitre();
 void MiseEnPlace();
 void FicheDeScore(WINDOW *Fenetre, int y, int x);
-void DeUn(WINDOW *Fenetre, int y, int x);
-void DeDeux(WINDOW *Fenetre, int y, int x);					
-void DeTrois(WINDOW *Fenetre, int y, int x);					
-void DeQuatre(WINDOW *Fenetre, int y, int x);				
-void DeCinq(WINDOW *Fenetre, int y, int x);					
-void DeSix(WINDOW *Fenetre, int y, int x);
-void DeVide(WINDOW *Fenetre, int y, int x);
-void AffichageDe(int De[5], WINDOW *Fenetre);
+void DeUn(int y, int x);
+void DeDeux(int y, int x);					
+void DeTrois(int y, int x);					
+void DeQuatre(int y, int x);				
+void DeCinq(int y, int x);					
+void DeSix(int y, int x);
+void DeVide(int y, int x);
+void AffichageDe(int De[5]);
 void ChargementAffichage();
 
 #endif
